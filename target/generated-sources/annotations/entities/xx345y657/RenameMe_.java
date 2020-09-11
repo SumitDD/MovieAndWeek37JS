ba902572cea3d668057bfc407ff3f5c729ca5e37
Sumit Dey <sumitdey0007@gmail.com>
2020-09-11 15:06:26 +0200

    FÆRDIG

--- target/generated-sources/annotations/entities/xx345y657/RenameMe_.java
+++ target/generated-sources/annotations/entities/xx345y657/RenameMe_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-08T20:09:32")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-11T14:57:36")
 @StaticMetamodel(RenameMe.class)
 public class RenameMe_ { 
 
